d172a406ab5082540512e3bae6578525941fd56c
emanuelboboiu <emanuelboboiu@gmail.com>
2023-01-11 22:16:14 +0200

    Version 2.1.1 after Google Play Store approved. Also implemented the statistics collection.

--- app/src/main/java/ro/pontes/pontesgamezone/Statistics.java
+++ app/src/main/java/ro/pontes/pontesgamezone/Statistics.java
@@ -2,77 +2,73 @@ package ro.pontes.pontesgamezone;
 
 /*
  * Class started on 24 September 2014 by Manu
- *Rewriten on 9 January 2023.
+ *Rewriten on 10 January 2023.
  * Methods for statistics, like postStatistics.
  */
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Statistics {
 
     // A method to post a new game and the number of hands played during the sessions:
     public static void postStats(final String gameIdInDB, final int numberOfGamesPlayed) {
-/*
-        // Run in another thread:
-        new Thread(new Runnable() {
-            public void run() {
-
-                // Create a new HttpClient and Post Header
-                HttpClient httpClient = new DefaultHttpClient();
-                HttpPost httppost = new HttpPost("http://www.pontes.ro/ro/divertisment/games/soft_counts.php");
-
-                try {
-                    // Add your data
-                    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-                    nameValuePairs.add(new BasicNameValuePair("pid", gameIdInDB));
-                    nameValuePairs.add(new BasicNameValuePair("score", "" + numberOfGamesPlayed));
-                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-                    // Execute HTTP Post Request
-                    HttpResponse response = httpClient.execute(httppost);
-                    response.toString();
-
-                } catch (ClientProtocolException e) {
-                    // TODO Auto-generated catch block
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                }
-
-            }
-        }).start();
-*/
-    } // end post data.
+        String url = "https://www.pontes.ro/ro/divertisment/games/soft_counts.php?pid=" + gameIdInDB + "&score=" + numberOfGamesPlayed;
+        new GetWebData().execute(url);
+    } // end post data statistics.
 
     // A method to post a new game and the number of hands played during the sessions:
     public static void postInsolvency(final String name, final int money) {
-/*
-        // Run in another thread:
-        new Thread(new Runnable() {
-            public void run() {
-
-                // Create a new HttpClient and Post Header
-                HttpClient httpClient = new DefaultHttpClient();
-                HttpPost httppost = new HttpPost("http://www.android.pontes.ro/pontesgamezone/stats/stats.php");
-
-                try {
-                    // Add your data
-                    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-                    nameValuePairs.add(new BasicNameValuePair("cont", MainActivity.userName));
-                    nameValuePairs.add(new BasicNameValuePair("nume", name));
-                    nameValuePairs.add(new BasicNameValuePair("suma", "" + money));
-                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-                    // Execute HTTP Post Request
-                    HttpResponse response = httpClient.execute(httppost);
-                    response.toString();
-
-                } catch (ClientProtocolException e) {
-                    // TODO Auto-generated catch block
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                }
-
-            }
-        }).start();
-*/
+        String url = "https://www.android.pontes.ro/pontesgamezone/stats/stats.php?cont=" + MainActivity.userName + "&nume=" + name + "&suma" + money;
+        new GetWebData().execute(url);
     } // end post data for insolvency.
+
+    // This is a subclass:
+    private static class GetWebData extends AsyncTask<String, String, String> {
+
+        // execute before task:
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        // Execute task
+        String urlText = "";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content = new StringBuilder();
+            urlText = strings[0];
+
+            try {
+                // Create a URL object:
+                URL url = new URL(urlText);
+                // Create a URLConnection object:
+                URLConnection urlConnection = url.openConnection();
+                // Wrap the URLConnection in a BufferedReader:
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream()));
+                String line;
+                // Read from the URLConnection via the BufferedReader:
+                while ((line = bufferedReader.readLine()) != null) {
+                    content.append(line);
+                }
+                bufferedReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return content.toString();
+        } // end doInBackground() method.
+
+        // Execute after task with the task result as string:
+        @Override
+        protected void onPostExecute(String s) {
+            // Do nothing yet.
+        } // end postExecute() method.
+    } // end subclass.
 
 } // end statistics class.
